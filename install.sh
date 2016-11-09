4b0a9cb51ca06aae4015e1708121509b3ff303b1
Stefan Rimaila <stefan@fusetools.com>
2016-11-09 12:06:49 +0100

    Update installation script, requirements

--- install.sh
+++ install.sh
@@ -9,9 +9,18 @@ if [[ ! -d "$DOTDIR" ]]; then
 
   if [[ -f "$HOME/.zshrc" ]]; then
     mv "$HOME/.zshrc" "$HOME/.zshrc.backup"
+    echo "I made a backup of your \`.zshrc\`, called `\.zshrc.backup`\. Just FYI!"
   fi
 
   ln -s "$DOTDIR/zsh/zshrc" "$HOME/.zshrc"
+
+  # Check if Homebrew is installed
+  if [[ ! -x $(which brew) ]]; then
+    echo "Installing Homebrew. This will ask for your password."
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+    echo "Homebrew installation done."
+  fi
 else
   echo "Already installed"
 fi
--- zsh/utils.sh
+++ zsh/utils.sh
@@ -6,3 +6,8 @@ RESET=$(tput sgr0)
 put_heading() { echo "${HEADING}${@}${RESET}"; }
 
 indent() { sed 's/^/    /'; }
+
+# Thanks! https://github.com/creationix/nvm/issues/539#issuecomment-110643090
+lazy_source () {
+    eval "$1 () { [ -f $2 ] && source $2 && $1 \$@ }"
+}
